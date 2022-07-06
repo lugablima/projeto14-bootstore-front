71052a27047b9d7973fdcb64cd49eb9b2c190a5d
LucasLima <lugablima@gmail.com>
2022-07-06 20:04:18 -0300

    chore: configuring eslint and prettier

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@
 
 function App() {
   // const API = process.env.REACT_APP_API_BASE_URL;
+  const oi = "olá";
 
   return (
     <div>
-      <h1>Essa é a URL da minha API</h1>
+      <h1>Essa é a URL da minha API {oi}</h1>
     </div>
   );
 }
