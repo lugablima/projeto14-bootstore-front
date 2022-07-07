8114be2394d3544d2ef3d94a59e4a879496fa1ee
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-07-06 23:14:52 -0300

    style: logo margin-bottom reduced

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -19,7 +19,7 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 130px;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
   background: transparent;
   font-family: "Bree Serif";
   font-style: normal;
