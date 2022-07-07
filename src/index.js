755141cdf22c1803fb730876bfa47aa384a85fe9
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-07-06 22:59:42 -0300

    fix: imported reset.css and style.css in main file index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App";
+import "./Assets/Styles/reset.css";
+import "./Assets/Styles/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
