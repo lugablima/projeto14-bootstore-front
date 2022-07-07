cde823a4a9b98afc564ebb5984c7ce207311f7fc
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-07-06 23:16:53 -0300

    feat: singup page layout implemented

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Login from "../Pages/Login";
+import SingUp from "../Pages/SingUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/singup" element={<SingUp />} />
       </Routes>
     </BrowserRouter>
   );
