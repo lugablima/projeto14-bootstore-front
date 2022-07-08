061f506d99d7d9bdc4e83dd3d1a14fd2ac4acf0f
LucasLima <lugablima@gmail.com>
2022-07-07 23:13:14 -0300

    chore: adjust code to merge with main branch

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,8 +3,6 @@ import Cart from "../Pages/Cart";
 import CartProvider from "../Contexts/CartContext";
 
 function App() {
-  // const API = process.env.REACT_APP_API_BASE_URL;
-
   return (
     <CartProvider>
       <BrowserRouter>
