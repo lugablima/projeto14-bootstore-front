43bf5b5d3056030e35c061e2a193f76a0fd5c87b
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-07-11 14:24:35 -0300

    Merge remote-tracking branch 'origin/main' into feature-store

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,21 +6,23 @@ import Login from "../Pages/Login";
 import SignUp from "../Pages/SignUp";
 import Store from "../Pages/Store";
 import Product from "../Pages/Product";
+import Cart from "../Pages/Cart";
 
 function App() {
   return (
-    <CartProvider>
-      <UserProvider>
+    <UserProvider>
+      <CartProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Store />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </BrowserRouter>
-      </UserProvider>
-    </CartProvider>
+      </CartProvider>
+    </UserProvider>
   );
 }
 
